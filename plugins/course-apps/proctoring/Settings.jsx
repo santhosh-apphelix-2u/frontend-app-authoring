d41ae7e8fcb786160d4fe7cb5db9ad6be4528540
Santhosh Kumar <skumar4-apphelix@2u.com>
2025-12-11 12:22:54 +0530

    chore: Refactor proctoring provider handling in Settings

--- plugins/course-apps/proctoring/Settings.jsx
+++ plugins/course-apps/proctoring/Settings.jsx
@@ -85,7 +85,6 @@ const ProctoringSettings = ({ onClose }) => {
       setFormValues({ ...formValues, [name]: value === 'true' });
     } else if (name === 'proctoringProvider') {
       const newFormValues = { ...formValues, proctoringProvider: value };
-
       if (requiresEscalationEmailProviders.includes(value)) {
         setFormValues({ ...newFormValues });
         setShowEscalationEmail(true);
